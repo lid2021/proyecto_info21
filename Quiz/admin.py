c6e418fd09d11e16accf4735d3ebce5a7e41778e
Leandro <leandrojosebenitez8@gmail.com>
2021-08-24 00:02:36 -0300

    Se agrego en admin 2 clases
    Para representar algo parecido al multiple choice

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -3,5 +3,18 @@ from .models import Preguntas, ElegirRespuesta
 
 # Register your models here.
 
-admin.site.register(Preguntas)
+class ElegirRespuestaInline(admin.TabularInline):
+    ''' Se hereda del campo elejirResuesta las respuestas de las preguntas, 
+    para listarlas dentro de la clase PreguntaAdmin como un multiple choices, con list_display'''
+    model = ElegirRespuesta
+    
+class PreguntaAdmin(admin.ModelAdmin):
+    model = Preguntas
+    inlines = (ElegirRespuestaInline, )
+    #Lista las respuestas heredadas de model ElegirRespusta
+    list_display = ['texto',]
+    search_fields = ['texto', 'pregunta__texto']
+
+
+admin.site.register(Preguntas, PreguntaAdmin)
 admin.site.register(ElegirRespuesta)
