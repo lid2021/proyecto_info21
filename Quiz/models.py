8c9f65a870e0739a4382d830af5726b59e8d4049
Leandro <leandrojosebenitez8@gmail.com>
2021-08-24 18:03:00 -0300

    Se agrego comentarios en las clases de models.py

--- Quiz/models.py
+++ Quiz/models.py
@@ -8,7 +8,7 @@ from django.db import models
 # se crea los Campos de DB
 
 class Preguntas(models.Model): #Se crea la caja de preguntas
-
+    '''Se cargan las preguntas desde el panel de admin y se conecta con la clase ElegirRespuestas con el campo texto'''
     #atributos de la clase
     texto = models.TextField(verbose_name='Texto de la pregunta')
 
@@ -17,10 +17,11 @@ class Preguntas(models.Model): #Se crea la caja de preguntas
         return self.texto
 
 class ElegirRespuesta(models.Model): #se crea y se polimorfea con la clase 'Pregunta'
- 
+    
     #Atributo de la clase
-    pregunta = models.ForeignKey(Preguntas, related_name='preguntas', on_delete=models.CASCADE)#Se limpia el campo despues de recargar la pregunta
-    #Para que la DB nos muestre el campo con ese nombre referencial
+    #Se limpia el campo despues de recargar la preguntaPara que la DB nos muestre el campo con ese nombre referencial
+    pregunta = models.ForeignKey(Preguntas, related_name='preguntas', on_delete=models.CASCADE)
+    #El campo booleano es para marcar la respuesta correcta al momento de cargar las preguntas
     correcta = models.BooleanField(verbose_name='¿Es la Pregunta Correcta?',default=False, null=False)
     texto = models.TextField(verbose_name='Texto de la Pregunta')
 
